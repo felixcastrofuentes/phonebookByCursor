c360ae6af543022361429800dfd3f4eb2108f702
Felix Castro <fcastro@gearlabs.cl>
2017-12-09 08:04:31 -0300

    listado de personas con busqueda por nombre y/o apellido

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+
+import { WspersonaProvider } from '../../providers/wspersona/wspersona';
+import { Persona } from '../../models/persona';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,81 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  personas: Persona[];
+  personasFilter: Persona[] = [];
+  refresher = null;
+  searchQuery: string = '';
+
+  constructor(public navCtrl: NavController, private wspersonaProvider: WspersonaProvider, public modalCtrl: ModalController) {
 
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PersonaDetallePage');
+    this.getPersonas();
+  }
+
+  /**
+   * Obtiene personas desde ws
+   */
+  getPersonas() {
+    console.log("obteniendo personas...");
+    this.wspersonaProvider.getPersonas()
+      .subscribe(personas => { this.personas = personas },
+      err => {
+        console.error("Error" + err.message);
+        if (this.refresher)
+          this.refresher.complete();
+      }
+      , () => {
+        console.log('COMPLETEEEE');
+        this.initializeItems();
+        if (this.refresher)
+          this.refresher.complete();
+
+      }
+      );
+  }
+
+
+  /**
+   * Refresh ionic
+   */
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+    this.refresher = refresher;
+    this.getPersonas();
+  }
+
+  /**
+   * Se inicializan items de personas para lista
+   */
+  initializeItems() {
+    this.personasFilter = [];
+
+    for (let entry of this.personas) {
+      if(entry.activo == true){
+        this.personasFilter.push(entry);
+      }
+    }
+  }
+
+  /**
+   * Filtra los items
+   */
+  filterItems(ev) {
+        this.initializeItems();
+        let val = ev.target.value;
+        if (val && val.trim() !== '') {
+            this.personasFilter = this.personasFilter.filter(function(item) {
+              if (item.apellido  && item.nombre)
+                return item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1 || item.apellido.toLowerCase().indexOf(val.toLowerCase()) > -1;
+              else
+              if (item.apellido)
+                return item.apellido.toLowerCase().indexOf(val.toLowerCase()) > -1;
+              else
+                return item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1;
+            });
+        }
+    }
 }
